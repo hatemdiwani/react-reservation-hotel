96a4eb71d9116f000255da9057ead1a7c15d2a83
hatem diwani <hatmen.dio@outlook.com>
2022-04-08 19:37:50 +0000

    fetch hotels

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Navbar from './layouts/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
@@ -12,8 +12,38 @@ import Hotels from './views/Hotels';
 import Footer from './layouts/Footer';
 import HotelSingle from './views/HotelSingle';
 import Profile from './views/Profile';
-
+import axios from 'axios'
 function App() {
+
+
+
+  useEffect(() => {
+    /* fetch('https://api.voyages2000.com.tn/hotels/availability?cityId=3&city=Hammamet&hotelId&checkIn=2022-04-02&checkOut=2022-04-09&pax=2')
+      .then(res => {
+        console.log(res);
+      }) */
+      
+      async function postData(url = 'https://api.voyages2000.com.tn/hotels/availability?cityId=3&city=Hammamet&hotelId&checkIn=2022-04-02&checkOut=2022-04-09&pax=2', data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+          method: 'GET', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'same-origin', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+         // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+      }
+      postData()
+  }, [])
+
+
   return (
     <div >
       <Router>
